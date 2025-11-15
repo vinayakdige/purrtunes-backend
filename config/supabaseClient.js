c0fbb923b6add0d443f0c5ad55356f30b4da90cc
vinayak dige <vinayakdige24@gmail.com>
2025-11-15 15:17:53 +0530

    URL changes

--- config/supabaseClient.js
+++ config/supabaseClient.js
@@ -2,7 +2,7 @@ const { createClient } = require('@supabase/supabase-js');
 require('dotenv').config();
 
 const supabaseUrl = process.env.SUPABASE_URL;
-const supabaseKey = process.env.SUPABASE_SERVICE_KEY;
+const supabaseKey = process.env.SUPABASE_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
